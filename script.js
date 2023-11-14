73661f6c4c9fbc6b88ada8f4fa9c57a8606200c8
aishatrue <140340401+aishatrue@users.noreply.github.com>
2023-11-14 21:47:54 +0900

    study

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriverLicense = true;
-if (hasDriversLicense) console.log("I can drive");
+// if (passTest) hasDriverLicense = true;
+// if (hasDriversLicense) console.log("I can drive");
+
+function fruitProcessor(orange, apple) {
+  const juice = `i love ${orange} and ${apple}`;
+  return juice;
+}
+
+let test = fruitProcessor(3, 0);
+console.log(test);
